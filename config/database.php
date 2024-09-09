c2d31e7dd569d4ef660b22c8bc1210cdb3ec1801
MatteoPetrault <matteo.petrault12@gmail.com>
2024-09-09 11:44:31 +0200

    migration db vers mysql

--- config/database.php
+++ config/database.php
@@ -18,6 +18,7 @@ return [
 
     'default' => env('DB_CONNECTION', 'sqlite'),
 
+
     /*
     |--------------------------------------------------------------------------
     | Database Connections
@@ -35,7 +36,7 @@ return [
             'driver' => 'sqlite',
             'url' => env('DB_URL'),
             'database' => env('DB_DATABASE', database_path('database.sqlite')),
-            'prefix' => '',
+            'prefix' => env('DB_TABLE_PREFIX', 'PFX_'),
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
             'busy_timeout' => null,
             'journal_mode' => null,
@@ -73,7 +74,7 @@ return [
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => env('DB_CHARSET', 'utf8mb4'),
             'collation' => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
-            'prefix' => '',
+            'prefix' => env('DB_TABLE_PREFIX', 'PFX_'),
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
